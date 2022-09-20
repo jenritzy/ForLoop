1f28c7c8c950091867acf0d66b7a3244cb9a684c
jenritzy <Jenritzy@outlook.com>
2022-09-20 13:44:24 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "ForLoop";
+            for (int i = 1; i < 4; i++) 
+            {
+                Console.WriteLine("Loop Iteration: " + i);
+                for(int j = 1; j < 4; j++)
+                {
+                    Console.WriteLine("\t inner loop iteration: " + j);
+                }
+            }
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
